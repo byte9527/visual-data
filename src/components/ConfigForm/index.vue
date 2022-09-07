d39f046ca7c4fdda35134544b6a363e93e5357ee
guyueyue <yuanweihua@douyu.tv>
2022-09-07 20:56:41 +0800

    初始化

---
+++ src/components/ConfigForm/index.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: zhangm
+ * @Date: 2022-09-07 20:42:16
+ * @LastEditTime: 2022-09-07 20:42:17
+ * @LastEditors: zhangm
+ * @Description: 
+ * @FilePath: \visual-data\src\components\ConfigForm\index.vue
+-->
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
