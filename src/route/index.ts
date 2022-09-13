61e86679b4c982850b8144327e4376b0d7af0e22
guyueyue <1026859692@qq.com>
2022-09-13 21:55:02 +0800

    no message

--- src/route/index.ts
+++ src/route/index.ts
@@ -22,62 +22,14 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Login',
     component: () => import('@/views/login/index.vue'),
     meta: { title: '登录' },
-  },
-  ...navRoutes,
-  {
-    path: '/create-screen',
-    name: 'CreateScreen',
-    component: () => import('@/views/my-project/create-screen.vue'),
-    meta: { title: '创建' },
-  },
-  {
-    path: '/admin/screen/:projectId',
-    name: 'ScreenEditor',
-    props: true,
-    component: () => import('@/views/screen-editor/index.vue'),
-    meta: { title: '编辑器' },
-  },
-  {
-    path: '/screen/preview/:screenId',
-    name: 'Preview',
-    props: true,
-    component: () => import('@/views/screen/index.vue'),
-  },
-  {
-    path: '/:catchAll(.*)*',
-    name: 'NotFound',
-    component: () => import('@/views/error/index.vue'),
-    meta: { title: '找不到页面' },
-  },
+  }
 ]
-
-if (__DEV__) {
-  routes.unshift(
-    {
-      path: '/dev/props-config',
-      name: 'DevPropsConfig',
-      component: () => import('@/pages/props-config/index.vue'),
-      meta: { title: '属性配置' },
-    },
-    {
-      path: '/dev/icons',
-      name: 'DevIcons',
-      component: () => import('@/pages/icons.vue'),
-      meta: { title: '全部图标' },
-    },
-  )
-}
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
 })
 
-
-const whiteList = ['/login', '/auth-redirect', '/dev'] 
-
-router.beforeEach(async (to, from, next) => {
-})
 
 
 export default router
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <router-view/>
+  <div>
+    <div v-for="item in routeList" :key="item.label">
+      <router-link to="/">{{item.label}}</router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 
 export default {
   name: 'home',
-  
+  data() {
+    return {
+      routeList: [
+        {
+          label: '配置表单',
+          route: '' 
+        }
+      ]
+    
+    }
+  }
 };
 
 </script>
